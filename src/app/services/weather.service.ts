154a45a947ad0fc63830040c05f073e88ca97795
Josh <jsbusiness305@gmail.com>
2022-09-03 00:57:57 -0400

    fix

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environment';
 import { WeatherData } from '../models/weather.model';
 
 @Injectable({
